e324f478189834249cf7b02bcbd4e140a9446d58
Azri <azriauliaakmal@gmail.com>
2025-07-13 20:41:40 +0700

    update name import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import Lihat from "./pages/Lihat";
 import Buat from "./pages/Buat";
 import DetailTrans from "./pages/DetailTrans";
