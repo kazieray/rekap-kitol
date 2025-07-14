053aa8be1b0d7ba21b89301c2b3ef52331823316
Azri <azriauliaakmal@gmail.com>
2025-07-14 10:53:33 +0700

    delete includeassets

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       tailwindcss(),
       VitePWA({
          registerType: "autoUpdate",
-         includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+         includeAssets: [],
          manifest: {
             name: "Rekap KITOL",
             short_name: "KITOL",
