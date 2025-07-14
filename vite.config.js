c26d787f5c3454fa485bc64425ecbef7febb2177
Azri <azriauliaakmal@gmail.com>
2025-07-14 10:51:29 +0700

    created pwa

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react(), tailwindcss()],
+   plugins: [
+      react(),
+      tailwindcss(),
+      VitePWA({
+         registerType: "autoUpdate",
+         includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
+         manifest: {
+            name: "Rekap KITOL",
+            short_name: "KITOL",
+            start_url: "/",
+            display: "standalone",
+            background_color: "#FBF0D8",
+            theme_color: "#0f172a",
+            orientation: "portrait",
+            icons: [
+               {
+                  src: "kitol-192.png",
+                  sizes: "192x192",
+                  type: "image/png",
+               },
+               {
+                  src: "kitol-512.png",
+                  sizes: "512x512",
+                  type: "image/png",
+               },
+            ],
+         },
+      }),
+   ],
 });
